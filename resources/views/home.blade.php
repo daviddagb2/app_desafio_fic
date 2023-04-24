5b308655ebd6ebd6fd8b51f357561f28de8554c4
David Gonzalez Blanchard <daviddagb2@gmail.com>
2023-04-24 11:55:33 -0600

    translations to home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,29 +17,26 @@
 @if($profile == null)
 <div class="px-4 py-5 my-5 text-center">
     <img class="d-block mx-auto mb-4" src="{{ url('/img/entrepreneur.png') }}" alt="" width="150" height="150">
-    <h2 class="display-5 fw-bold titleconfig">Hemos detectado que aun no configuras tu perfil de emprendedor</h2>
+    <h2 class="display-5 fw-bold titleconfig">{{ __('We have detected that you have not yet configured your entrepreneur profile')}}</h2>
     <div class="col-lg-6 mx-auto">
       <p class="lead mb-4">
-        ¡Aún no has configurado tu perfil de emprendedor! Completa tu información para tener acceso a todas las herramientas y recursos que ofrecemos.
+        {{  __('You haven't set up your entrepreneur profile yet! Complete your information to have access to all the tools and resources we offer.') }}
       </p>
       <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <a href="{{ URL::route('getstarted'); }}" class="btn btn-primary btn-lg px-4 gap-3">Completar perfil</a>
+        <a href="{{ URL::route('getstarted'); }}" class="btn btn-primary btn-lg px-4 gap-3">{{ __('Complete profile') }}</a>
       </div>
     </div>
 </div>
 
-
 @else
-
-
 
 <div class="px-4 my-5 text-center">
     <img class="d-block mx-auto mb-4" src="{{ url('/img/entrepreneur.png') }}" alt="" width="150" height="150">
-    <h2 class="display-5 fw-bold titleconfig">Hola {{ $profile->name }}</h2>
-    <h2 class="display-5 fw-bold titleconfig">Bienvenido a tu perfil Emprendedor</h2>
+    <h2 class="display-5 fw-bold titleconfig">{{ __('Hi')}} {{ $profile->name }}</h2>
+    <h2 class="display-5 fw-bold titleconfig">{{ __('Welcome to your Entrepreneur profile') }}</h2>
     <div class="col-lg-6 mx-auto">
         <p class="lead mb-4">
-        Para poner en marcha tu negocio se requieren los siguients elementos:
+        {{ __('To start your business, the following elements are required') }}
         </p>
       </div>
   </div>
@@ -53,28 +50,26 @@
               <div class="col-md-4">
                 <div class="step">
                   <div class="step-number">1</div>
-                  <div class="step-title">Constituir una empresa</div>
+                  <div class="step-title">{{ __('Establish a company') }}</div>
                   <div class="step-description">
-                  
-                  Para constituir tu empresa entra al siguiente enlace: <a href="https://www.registrodeempresasysociedades.cl/" target="_blank"> https://www.registrodeempresasysociedades.cl/</a>
-
+                  {{ __('To set up your company go to the following link') }}: <a href="https://www.registrodeempresasysociedades.cl/" target="_blank"> https://www.registrodeempresasysociedades.cl/</a>
                   </div>
                 </div>
               </div>
               <div class="col-md-4">
                 <div class="step">
                   <div class="step-number">2</div>
-                  <div class="step-title">Formalizar Ante el SII</div>
+                  <div class="step-title">{{ __('Formalize Before the SII') }}</div>
                   <div class="step-description">
-                  Crea tu cuenta de usuario en el portal del SII e ingresa la información solicitada. <a href="https://homer.sii.cl/" target="_blank"> https://homer.sii.cl/ </a>
+                  {{ __('Create your user account on the SII portal and enter the requested information') }}. <a href="https://homer.sii.cl/" target="_blank"> https://homer.sii.cl/ </a>
                   </div>
                 </div>
               </div>
               <div class="col-md-4">
                 <div class="step">
                   <div class="step-number">3</div>
-                  <div class="step-title">Presentar la información del municipio</div>
-                  <div class="step-description">Debes dirigirte al municipio de tu comuna y presenta la documentación solicitada.</div>
+                  <div class="step-title">{{ __('Present the information of the municipality') }}</div>
+                  <div class="step-description">{{ __('You must go to the municipality of your commune and present the requested documentation.')}}</div>
                 </div>
               </div>
             </div>
