89e2fbae97215265fff4ccb119fa22d47f8dc179
David Gonzalez Blanchard <daviddagb2@gmail.com>
2023-04-27 09:32:37 -0600

    update to database script

--- Dump20230423.sql
+++ Dump20230423.sql
@@ -37,7 +37,7 @@ CREATE TABLE `communes` (
 
 LOCK TABLES `communes` WRITE;
 /*!40000 ALTER TABLE `communes` DISABLE KEYS */;
-INSERT INTO `communes` VALUES (1,'Las Condes','2023-04-22 23:21:01','2023-04-22 23:21:01'),(2,'Puente Alto','2023-04-22 23:21:01','2023-04-22 23:21:01'),(3,'La Florida','2023-04-22 23:21:01','2023-04-22 23:21:01');
+INSERT INTO `communes` VALUES (1,'Las Condes','2023-04-26 17:48:50','2023-04-26 17:48:50'),(2,'Puente Alto','2023-04-26 17:48:50','2023-04-26 17:48:50'),(3,'La Florida','2023-04-26 17:48:50','2023-04-26 17:48:50');
 /*!40000 ALTER TABLE `communes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -71,7 +71,7 @@ CREATE TABLE `document_assignments` (
 
 LOCK TABLES `document_assignments` WRITE;
 /*!40000 ALTER TABLE `document_assignments` DISABLE KEYS */;
-INSERT INTO `document_assignments` VALUES (1,1,1,2,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(2,2,1,2,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(3,3,1,2,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(4,4,1,1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(5,5,1,1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(6,6,1,3,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(7,7,1,3,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(8,1,2,2,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(9,8,2,2,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(10,3,2,2,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(11,4,2,1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(12,5,2,1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(13,9,2,1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(14,6,2,3,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(15,10,2,3,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(16,9,3,2,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(17,11,3,2,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(18,12,3,2,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(19,4,3,1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(20,5,3,1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(21,9,3,1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(22,6,3,3,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(23,10,3,3,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(24,9,3,3,'2023-04-22 23:21:01','2023-04-22 23:21:01');
+INSERT INTO `document_assignments` VALUES (1,1,1,2,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(2,2,1,2,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(3,3,1,2,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(4,4,1,1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(5,5,1,1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(6,6,1,3,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(7,7,1,3,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(8,1,2,2,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(9,8,2,2,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(10,3,2,2,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(11,4,2,1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(12,5,2,1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(13,9,2,1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(14,6,2,3,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(15,10,2,3,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(16,9,3,2,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(17,11,3,2,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(18,12,3,2,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(19,4,3,1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(20,5,3,1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(21,9,3,1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(22,6,3,3,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(23,10,3,3,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(24,9,3,3,'2023-04-26 17:48:50','2023-04-26 17:48:50');
 /*!40000 ALTER TABLE `document_assignments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -99,7 +99,7 @@ CREATE TABLE `documents` (
 
 LOCK TABLES `documents` WRITE;
 /*!40000 ALTER TABLE `documents` DISABLE KEYS */;
-INSERT INTO `documents` VALUES (1,'Permiso de obras','Permiso de obras',1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(2,'Permiso de operatividad','Permiso de operatividad',1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(3,'Conformación de la sociedad','Documento de Conformación de la sociedad',1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(4,'Permiso de manufactura de alimentos','Documento de Permiso de manufactura de alimentos',1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(5,'Permiso de autorización Sanitaria','Documento de Permiso de autorización Sanitaria',1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(6,'Permisos de importación','Documento de Permisos de importación',1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(7,'Patentes legales de productos en venta','Documento de Patentes legales de productos en venta',1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(8,'Permiso de manipulación de alimentos','Documento de Permiso de manipulación de alimentos',1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(9,'Cédula de identidad','Documento Cédula de identidad',1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(10,'Patente de manufactura de proveedores','Documento de Patente de manufactura de proveedores',1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(11,'Copia de traslado inicial de mercaderías','Documento Copia de traslado inicial de mercaderías',1,'2023-04-22 23:21:01','2023-04-22 23:21:01'),(12,'Manual de buenas prácticas para la comercialización de productos','Documento manual de buenas prácticas para la comercialización de productos',1,'2023-04-22 23:21:01','2023-04-22 23:21:01');
+INSERT INTO `documents` VALUES (1,'Permiso de obras','Permiso de obras',1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(2,'Permiso de operatividad','Permiso de operatividad',1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(3,'Conformación de la sociedad','Documento de Conformación de la sociedad',1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(4,'Permiso de manufactura de alimentos','Documento de Permiso de manufactura de alimentos',1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(5,'Permiso de autorización Sanitaria','Documento de Permiso de autorización Sanitaria',1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(6,'Permisos de importación','Documento de Permisos de importación',1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(7,'Patentes legales de productos en venta','Documento de Patentes legales de productos en venta',1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(8,'Permiso de manipulación de alimentos','Documento de Permiso de manipulación de alimentos',1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(9,'Cédula de identidad','Documento Cédula de identidad',1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(10,'Patente de manufactura de proveedores','Documento de Patente de manufactura de proveedores',1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(11,'Copia de traslado inicial de mercaderías','Documento Copia de traslado inicial de mercaderías',1,'2023-04-26 17:48:50','2023-04-26 17:48:50'),(12,'Manual de buenas prácticas para la comercialización de productos','Documento manual de buenas prácticas para la comercialización de productos',1,'2023-04-26 17:48:50','2023-04-26 17:48:50');
 /*!40000 ALTER TABLE `documents` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -158,7 +158,7 @@ CREATE TABLE `entrepreneurs` (
   CONSTRAINT `entrepreneurs_commune_id_foreign` FOREIGN KEY (`commune_id`) REFERENCES `communes` (`id`),
   CONSTRAINT `entrepreneurs_industry_sector_id_foreign` FOREIGN KEY (`industry_sector_id`) REFERENCES `industry_sectors` (`id`),
   CONSTRAINT `entrepreneurs_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -167,7 +167,7 @@ CREATE TABLE `entrepreneurs` (
 
 LOCK TABLES `entrepreneurs` WRITE;
 /*!40000 ALTER TABLE `entrepreneurs` DISABLE KEYS */;
-INSERT INTO `entrepreneurs` VALUES (1,'Pasteles Miriam','8888888','Managua Nicaragua',11,1,1,'2023-04-24 05:45:32','2023-04-24 10:26:37');
+INSERT INTO `entrepreneurs` VALUES (1,'Super Express','8888888','Matagalpa',11,1,2,'2023-04-26 21:19:05','2023-04-27 03:46:30'),(2,'Pasteleria Mil Flores','84803590','Ciudad el Doral',11,2,1,'2023-04-27 02:05:25','2023-04-27 03:13:40'),(3,'Celulares Chavelo','8282828282','Matagalpa',11,3,3,'2023-04-27 03:44:58','2023-04-27 03:44:58'),(4,'Pasteles doña Coco','8282829292','Masaya',11,2,1,'2023-04-27 03:47:07','2023-04-27 03:47:07'),(5,'Nica Business','92929292922','Matagalpa',11,2,2,'2023-04-27 03:48:02','2023-04-27 03:48:02'),(6,'The Lab','92929292922','Matagalpa',11,2,2,'2023-04-27 03:48:15','2023-04-27 03:48:15');
 /*!40000 ALTER TABLE `entrepreneurs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -223,7 +223,7 @@ CREATE TABLE `industry_sectors` (
 
 LOCK TABLES `industry_sectors` WRITE;
 /*!40000 ALTER TABLE `industry_sectors` DISABLE KEYS */;
-INSERT INTO `industry_sectors` VALUES (1,'Pastelería','Panadería que se especializa en productos horneados dulces, pasteles y postres','2023-04-22 23:21:01','2023-04-22 23:21:01'),(2,'Minimarket','Pequeña tienda de abarrotes que vende una variedad de alimentos y artículos del hogar','2023-04-22 23:21:01','2023-04-22 23:21:01'),(3,'Electrónica','Tiendas que venden dispositivos y gadgets electrónicos, como computadoras, smartphones y televisores','2023-04-22 23:21:01','2023-04-22 23:21:01');
+INSERT INTO `industry_sectors` VALUES (1,'Pastelería','Panadería que se especializa en productos horneados dulces, pasteles y postres','2023-04-26 17:48:50','2023-04-26 17:48:50'),(2,'Minimarket','Pequeña tienda de abarrotes que vende una variedad de alimentos y artículos del hogar','2023-04-26 17:48:50','2023-04-26 17:48:50'),(3,'Electrónica','Tiendas que venden dispositivos y gadgets electrónicos, como computadoras, smartphones y televisores','2023-04-26 17:48:50','2023-04-26 17:48:50');
 /*!40000 ALTER TABLE `industry_sectors` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -239,7 +239,7 @@ CREATE TABLE `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -248,7 +248,7 @@ CREATE TABLE `migrations` (
 
 LOCK TABLES `migrations` WRITE;
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-INSERT INTO `migrations` VALUES (34,'2014_10_12_000000_create_roles_table',1),(35,'2014_10_12_000000_create_users_table',1),(36,'2014_10_12_100000_create_password_reset_tokens_table',1),(37,'2019_08_19_000000_create_failed_jobs_table',1),(38,'2019_12_14_000001_create_personal_access_tokens_table',1),(39,'2023_04_22_033703_create_industry_sectors_table',1),(40,'2023_04_22_033732_create_communes_table',1),(41,'2023_04_22_033733_create_entrepreneurs_table',1),(42,'2023_04_22_033811_create_documents_table',1),(43,'2023_04_22_153950_create_document_assignments_table',1),(44,'2023_04_22_153950_create_entrepreneur_documents_table',1);
+INSERT INTO `migrations` VALUES (46,'2014_10_12_000000_create_roles_table',1),(47,'2014_10_12_000000_create_users_table',1),(48,'2014_10_12_100000_create_password_reset_tokens_table',1),(49,'2019_08_19_000000_create_failed_jobs_table',1),(50,'2019_12_14_000001_create_personal_access_tokens_table',1),(51,'2023_04_22_033703_create_industry_sectors_table',1),(52,'2023_04_22_033732_create_communes_table',1),(53,'2023_04_22_033733_create_entrepreneurs_table',1),(54,'2023_04_22_033811_create_documents_table',1),(55,'2023_04_22_153950_create_document_assignments_table',1),(56,'2023_04_22_153950_create_entrepreneur_documents_table',1),(57,'2023_04_25_092032_add_language_to_users_table',1);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -331,7 +331,7 @@ CREATE TABLE `roles` (
 
 LOCK TABLES `roles` WRITE;
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,'Admin','2023-04-22 23:21:01','2023-04-22 23:21:01'),(2,'Vendedor','2023-04-22 23:21:01','2023-04-22 23:21:01');
+INSERT INTO `roles` VALUES (1,'Admin','2023-04-26 17:48:50','2023-04-26 17:48:50'),(2,'Vendedor','2023-04-26 17:48:50','2023-04-26 17:48:50');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -348,7 +348,8 @@ CREATE TABLE `users` (
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `role_id` bigint unsigned NOT NULL,
+  `language` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT 'es',
+  `role_id` bigint unsigned DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
@@ -365,7 +366,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Jayne Marks MD','sean07@example.net','2023-04-22 23:21:01','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',2,'xVsTSNB2gh','2023-04-22 23:21:01','2023-04-22 23:21:01'),(2,'Brad Ernser','lemke.bailey@example.com','2023-04-22 23:21:01','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',2,'EoJzS08Gbl','2023-04-22 23:21:01','2023-04-22 23:21:01'),(3,'Dominic Beatty I','rpollich@example.org','2023-04-22 23:21:01','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',2,'mmVBczmwQG','2023-04-22 23:21:01','2023-04-22 23:21:01'),(4,'Marcellus Rosenbaum III','rafaela63@example.net','2023-04-22 23:21:01','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',2,'sjRor1v8fl','2023-04-22 23:21:01','2023-04-22 23:21:01'),(5,'Dr. Rhoda Corwin','wava02@example.org','2023-04-22 23:21:01','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',2,'Hc4kkxYFTB','2023-04-22 23:21:01','2023-04-22 23:21:01'),(6,'Mr. Enos Daugherty','kemmer.barney@example.com','2023-04-22 23:21:01','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',2,'TErKQAnd4L','2023-04-22 23:21:01','2023-04-22 23:21:01'),(7,'Ms. Fay Herman DDS','lila42@example.com','2023-04-22 23:21:01','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',2,'WpPlbnDm2l','2023-04-22 23:21:01','2023-04-22 23:21:01'),(8,'Shanelle McGlynn','towne.reymundo@example.com','2023-04-22 23:21:01','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',2,'9OeF4EMQNN','2023-04-22 23:21:01','2023-04-22 23:21:01'),(9,'Bessie Harris','huels.kiana@example.net','2023-04-22 23:21:01','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',2,'1pa1eDpVjv','2023-04-22 23:21:01','2023-04-22 23:21:01'),(10,'Kiley Torp','svandervort@example.net','2023-04-22 23:21:01','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',2,'GKlwC2rtH1','2023-04-22 23:21:01','2023-04-22 23:21:01'),(11,'David','2jshkug0e@mozmail.com','2023-04-22 23:21:01','$2y$10$m/mOKxmqk7JnrkJm5DjkI.mYbnUIPX0iRPFq/RcVNx29SyuJ958fO',1,'Wni8DwyuE4JAzqW6uoFTS5QGIbYVZq720etcc0EWP8Sp4fsjlmh2sjAYVjmK','2023-04-22 23:21:01','2023-04-22 23:21:01');
+INSERT INTO `users` VALUES (1,'Price Schmeler','gconn@example.com','2023-04-26 17:48:50','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','es',2,'QKHrdF5ogn','2023-04-26 17:48:50','2023-04-26 17:48:50'),(2,'Margarete Ernser IV','grimes.ramona@example.org','2023-04-26 17:48:50','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','es',2,'NcwGcgLbXs','2023-04-26 17:48:50','2023-04-26 17:48:50'),(3,'Chester Boehm','stoltenberg.josianne@example.org','2023-04-26 17:48:50','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','es',2,'77vQyW2U18','2023-04-26 17:48:50','2023-04-26 17:48:50'),(4,'Prof. Edmund Kuhlman DDS','maymie.rau@example.net','2023-04-26 17:48:50','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','es',2,'zjgJkjI8nt','2023-04-26 17:48:50','2023-04-26 17:48:50'),(5,'Yadira Sporer','wkuhlman@example.org','2023-04-26 17:48:50','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','es',2,'AdnZsvwy03','2023-04-26 17:48:50','2023-04-26 17:48:50'),(6,'Gussie Bergnaum','metz.libby@example.org','2023-04-26 17:48:50','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','es',2,'M4c9uoBmQH','2023-04-26 17:48:50','2023-04-26 17:48:50'),(7,'Dr. Theron Bahringer DDS','whitney70@example.net','2023-04-26 17:48:50','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','es',2,'4Q4XNPjER7','2023-04-26 17:48:50','2023-04-26 17:48:50'),(8,'Anabelle Green','marcos70@example.com','2023-04-26 17:48:50','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','es',2,'SsuhYud5lQ','2023-04-26 17:48:50','2023-04-26 17:48:50'),(9,'Giovanni Marks','kaya.gerhold@example.org','2023-04-26 17:48:50','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','es',2,'cfbNvDiA9c','2023-04-26 17:48:50','2023-04-26 17:48:50'),(10,'Camron Cremin','skylar.jaskolski@example.com','2023-04-26 17:48:50','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','es',2,'0P9saXhOHC','2023-04-26 17:48:50','2023-04-26 17:48:50'),(11,'David','2jshkug0e@mozmail.com','2023-04-26 17:48:50','$2y$10$3epSi8JFUdquc7izNeaUieoqvvjplg6kL.gvDR0JkhwNp80PYjdiy','en',1,'N2mvp0347fUxIcd5nizN929cVYae6mVJO5uHf0nTMEUVFRg90lf9YHrfiwa0','2023-04-26 17:48:50','2023-04-27 15:28:49');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -378,4 +379,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-23 23:12:44
+-- Dump completed on 2023-04-27  9:31:56
